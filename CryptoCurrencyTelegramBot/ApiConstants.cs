81c0a01944b29b2a92402f7bebda162143db6fb2
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-01-13 16:57:41 +0500

    Init

---
+++ CryptoCurrencyTelegramBot/ApiConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoCurrencyTelegramBot;
+/// <summary>
+/// Class to save Api Keys
+/// </summary>
+public static class ApiConstants
+{
+    public const string BOT_API = "Your Bot Api";
+    public const string COIN_MARKET_API = "CoinMarket Api";
+}
